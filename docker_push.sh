470fc6b89b13ce164552968c47183096b15f92a3
Alexander Akulov <alexakulov86@gmail.com>
2019-07-09 17:42:56 +0500

    fix docker release

--- docker_push.sh
+++ docker_push.sh
@@ -3,6 +3,7 @@
 echo "${DOCKER_PASSWORD}" | docker login -u alexakulov --password-stdin
 
 if [ "$1" == "release" ]; then
+    docker tag "alexakulov/clickhouse-backup:latest" "alexakulov/clickhouse-backup:${TRAVIS_TAG//v}"
     docker push "alexakulov/clickhouse-backup:${TRAVIS_TAG//v}"
 else
     docker push "alexakulov/clickhouse-backup:latest"
