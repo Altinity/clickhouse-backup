b3e2595950f3e16f36610fc714dc65b074160ad4
Alexander Akulov <alexakulov86@gmail.com>
2019-09-26 12:49:25 +0500

    Docker. 'latest' tag refers to latest release and 'master' tag added

--- docker_push.sh
+++ docker_push.sh
@@ -2,7 +2,7 @@
 
 echo "${DOCKER_PASSWORD}" | docker login -u alexakulov --password-stdin
 
-docker push "alexakulov/clickhouse-backup:latest"
+docker push "alexakulov/clickhouse-backup:master"
 
 if [ "$1" == "release" ]; then
     docker tag "alexakulov/clickhouse-backup:latest" "alexakulov/clickhouse-backup:${TRAVIS_TAG//v}"
