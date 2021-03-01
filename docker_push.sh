5f25aad71036a466fdffc16b51ebe5feba600dc5
AlexAkulov <alexakulov86@gmail.com>
2021-03-01 17:27:41 +0500

    Fix old-format backup name in list

--- docker_push.sh
+++ docker_push.sh
@@ -10,8 +10,8 @@ echo "${DOCKER_PASSWORD}" | docker login -u alexakulov --password-stdin
 docker push "alexakulov/clickhouse-backup:${TRAVIS_BRANCH}"
 
 if [ "$1" == "release" ]; then
-    docker tag "alexakulov/clickhouse-backup:master" "alexakulov/clickhouse-backup:${TRAVIS_TAG//v}"
-    docker tag "alexakulov/clickhouse-backup:master" "alexakulov/clickhouse-backup:latest"
+    docker tag "alexakulov/clickhouse-backup:${TRAVIS_BRANCH}" "alexakulov/clickhouse-backup:${TRAVIS_TAG//v}"
+    docker tag "alexakulov/clickhouse-backup:${TRAVIS_BRANCH}" "alexakulov/clickhouse-backup:latest"
     docker push "alexakulov/clickhouse-backup:${TRAVIS_TAG//v}"
     docker push "alexakulov/clickhouse-backup:latest"
 fi
