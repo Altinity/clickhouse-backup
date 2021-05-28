203833dac761414f0b56cb1d8c3d3073b0f856c3
Valeriy Pastushenko <i@combin.name>
2021-05-28 14:30:16 +0300

    Resolve conflicts

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -1,12 +1,21 @@
-#/bin/bash
+#!/bin/bash
 set -x
 set -e
 
-GOOS=linux GOARCH=amd64 go build ./cmd/clickhouse-backup
-docker-compose -f test/integration/${COMPOSE_FILE:-docker-compose_storage-policy.yml} down
-docker-compose -f test/integration/${COMPOSE_FILE:-docker-compose_storage-policy.yml} up -d
+export CLICKHOUSE_BACKUP_BIN="$(pwd)/clickhouse-backup/clickhouse-backup"
+export LOG_LEVEL=${LOG_LEVEL:-info}
+
+if [[ "${CLICKHOUSE_VERSION:-21.3}" == 2* ]]; then
+  export COMPOSE_FILE=docker-compose_advanced.yml
+else
+  export COMPOSE_FILE=docker-compose.yml
+fi
+
+docker-compose -f test/integration/${COMPOSE_FILE} down --remove-orphans
+make clean
+make build
+docker-compose -f test/integration/${COMPOSE_FILE} up -d --force-recreate
 
 # To run integration tests including GCS tests set GCS_TESTS environment variable:
-# GCS_TESTS=true go test -tags=integration -v
+# GCS_TESTS=true go test -tags=integration -v test/integration/integration_test.go
 go test -tags=integration -v test/integration/integration_test.go
-# docker-compose -f docker-compose.yml down
