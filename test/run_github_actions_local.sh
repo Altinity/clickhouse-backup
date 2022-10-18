fc8aaf992fe51654162d01c4f8aa768db7d00ffd
Slach <bloodjazman@gmail.com>
2022-10-18 11:21:21 +0300

    continue debug build.yaml why clickhouse container doesn't start
    Signed-off-by: Slach <bloodjazman@gmail.com>

---
+++ test/run_github_actions_local.sh
@@ -0,0 +1,11 @@
+# require curl -sSLf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash -x
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "${CUR_DIR}/testflows/.env"
+act "pull_request" -r -v -b -C "${CUR_DIR}/../" \
+  -s VAULT_PASSWORD="${VAULT_PASSWORD}" \
+  -s QA_AWS_ACCESS_KEY="${QA_AWS_ACCESS_KEY}" \
+  -s QA_AWS_ENDPOINT="${QA_AWS_ENDPOINT}" \
+  -s QA_AWS_SECRET_KEY="${QA_AWS_SECRET_KEY}" \
+  -s QA_AWS_REGION="${QA_AWS_REGION}" \
+  -s QA_AWS_BUCKET="${QA_AWS_BUCKET}" \
+  -s QA_GCS_CRED_JSON="${QA_GCS_CRED_JSON}"
