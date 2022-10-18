400d6577b138adde7901fcb0112e425dce09bed1
Slach <bloodjazman@gmail.com>
2022-10-18 13:35:13 +0300

    add run_github_actions_local.sh
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- test/run_github_actions_local.sh
+++ test/run_github_actions_local.sh
@@ -1,7 +1,9 @@
 # require curl -sSLf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash -x
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+mkdir -p /tmp/act/artifacts
 source "${CUR_DIR}/testflows/.env"
-act "pull_request" -r -v -b -C "${CUR_DIR}/../" \
+act "pull_request" -r -b -C "${CUR_DIR}/../" \
+  --artifact-server-path /tmp/act/artifacts \
   -s VAULT_PASSWORD="${VAULT_PASSWORD}" \
   -s QA_AWS_ACCESS_KEY="${QA_AWS_ACCESS_KEY}" \
   -s QA_AWS_ENDPOINT="${QA_AWS_ENDPOINT}" \
