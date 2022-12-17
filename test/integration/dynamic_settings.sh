4ceff667f6fa767a18dac688684b2160ef3161c0
Slach <bloodjazman@gmail.com>
2022-12-17 10:21:53 +0400

    21.8 also support only `allow_experimental_database_materialize_mysql`
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- test/integration/dynamic_settings.sh
+++ test/integration/dynamic_settings.sh
@@ -207,7 +207,7 @@ EOT
 
 fi
 
-if [[ "${CLICKHOUSE_VERSION}" =~ ^21\.[3-9] || ]"${CLICKHOUSE_VERSION}" =~ ^21\.1[0-9] ]; then
+if [[ "${CLICKHOUSE_VERSION}" =~ ^21\.[3-9] || "${CLICKHOUSE_VERSION}" =~ ^21\.1[0-9] ]]; then
 cat <<EOT > /etc/clickhouse-server/users.d/allow_experimental_database_materialize_mysql.xml
 <yandex>
 <profiles><default>
