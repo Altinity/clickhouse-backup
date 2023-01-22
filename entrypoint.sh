a4e417c41fb7493b2cf5b965cc3c564705816e16
Darshil shah <darshil.shah@greendeck.co>
2023-01-22 14:34:13 +0530

    fixed bug for watch as a arg in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]] || [[ ! -x  $(command -v "$1") ]]; then
+if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]] || [[ ! -x  $(command -v "$1") ]] || [[ "$1" == "watch" ]]; then
     exec /bin/clickhouse-backup "$@"
 fi
 exec "$@"
