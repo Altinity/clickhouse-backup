19730f4dd52f5c3d807ba83e75dd632dc09121ea
Slach <bloodjazman@gmail.com>
2023-01-22 14:13:49 +0400

    small fixes for kopia

--- test/integration/kopia/delete.sh
+++ test/integration/kopia/delete.sh
@@ -5,5 +5,5 @@ source "${CUR_DIR}/init.sh"
 BACKUP_NAME=$1
 snapshot_ids=$(kopia snapshot list --json --tags="backup_name:${BACKUP_NAME}" | jq -c -r -M '.[].id')
 for snapshot_id in ${snapshot_ids}; do
-  kopia snapshot delete "${snapshot_id}"
+  kopia snapshot delete --delete "${snapshot_id}"
 done
--- test/integration/kopia/list.sh
+++ test/integration/kopia/list.sh
@@ -3,4 +3,4 @@ set +x
 set -euo pipefail
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "${CUR_DIR}/init.sh" &>/dev/null
-kopia snapshot list --storage-stats --json | jq -c -M '.[] | {"snapshot_id": .id, "snapshot_path": .source.path, "backup_name": .tags["tag:backup_name"], "creation_date": .endTime, "data_size": .storageStats.runningTotal.originalContentBytes, "metadata_size": 0 }'
+kopia snapshot list --storage-stats --json | jq -c -M '.[] | {"snapshot_id": .id, "snapshot_path": .source.path, "backup_name": .tags["tag:backup_name"], "creation_date": .endTime, "data_size": .storageStats.newData.packedContentBytes, "metadata_size": 0 }'
