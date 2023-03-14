eeccae1d33fd354c8564b5ed8a3e5ee456fff9cf
Slach <bloodjazman@gmail.com>
2023-03-14 21:17:31 +0400

    replace `yandex/clickhouse-operator` to `clickhouse/clickhouse-operator`, cause repo.clickhouse.tech return error during apt-get upgrade

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -5,7 +5,7 @@ set -e
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 export CLICKHOUSE_VERSION=${CLICKHOUSE_VERSION:-22.8}
-if [[ "${CLICKHOUSE_VERSION}" =~ 2[2-9]+ || "${CLICKHOUSE_VERSION}" == "head" ]]; then
+if [[ "${CLICKHOUSE_VERSION}" =~ ^2[1-9]+ || "${CLICKHOUSE_VERSION}" == "head" ]]; then
   export CLICKHOUSE_IMAGE=${CLICKHOUSE_IMAGE:-clickhouse/clickhouse-server}
 else
   export CLICKHOUSE_IMAGE=${CLICKHOUSE_IMAGE:-yandex/clickhouse-server}
