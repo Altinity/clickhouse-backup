f224f1ed1dba065e10454bed44ed8fe116a92add
Slach <bloodjazman@gmail.com>
2023-03-29 22:29:25 +0400

    looks like clickhouse/clickhouse-keeper:latest-alpine works unstable

--- test/integration/minio_nodelete.sh
+++ test/integration/minio_nodelete.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 mc admin user add local nodelete nodelete_password
-mc admin policy add local nodelete <( cat << EOF
+mc admin policy create local nodelete <( cat << EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -29,6 +29,6 @@ mc admin policy add local nodelete <( cat << EOF
 EOF
 )
 
-mc admin policy set local nodelete user=nodelete
+mc admin policy attach local nodelete --user nodelete
 mc alias set nodelete http://localhost:9000 nodelete nodelete_password
 mc alias list
