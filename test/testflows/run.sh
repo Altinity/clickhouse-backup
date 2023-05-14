a45d5cd3af9d5587d7557e095e7e7137be96fd23
Slach <bloodjazman@gmail.com>
2023-05-14 23:49:29 +0400

    fixes for testflows and coverage

--- test/testflows/run.sh
+++ test/testflows/run.sh
@@ -2,4 +2,4 @@ CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 mkdir -p "${CUR_DIR}/_coverage_/"
 source "${CUR_DIR}/.env"
 make clean build-race-docker
-python3 "${CUR_DIR}/clickhouse_backup/regression.py" --debug --only=${RUN_TESTS:-*}"
+python3 "${CUR_DIR}/clickhouse_backup/regression.py" --debug --only="${RUN_TESTS:-*}"
