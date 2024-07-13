c0841f5a402633aa997ea65db5ecfa8182762263
Slach <bloodjazman@gmail.com>
2024-07-13 11:12:28 +0400

    add more informative error message for Config.SetPriority, fix https://github.com/Altinity/clickhouse-backup/issues/953

--- pkg/config/config_linux.go
+++ pkg/config/config_linux.go
@@ -3,21 +3,27 @@ package config
 import (
 	"github.com/apex/log"
 	"github.com/xyproto/gionice"
+	"os"
 )
 
 func (cfg *Config) SetPriority() error {
 	var err error
+	var executable string
 	if cfg.General.IONicePriority != "" {
 		var nicePriority gionice.PriClass
+		executable, err = os.Executable()
+		if err != nil {
+			log.Warnf("can't get current executable path: %v", err)
+		}
 		if nicePriority, err = gionice.Parse(cfg.General.IONicePriority); err != nil {
 			return err
 		}
 		if err = gionice.SetIDPri(0, nicePriority, 7, gionice.IOPRIO_WHO_PGRP); err != nil {
-			log.Warnf("can't set i/o priority %s, error: %v", cfg.General.IONicePriority, err)
+			log.Warnf("can't set i/o priority %s, error: %v, use `sudo setcap cap_sys_nice+ep %s` to fix it", cfg.General.IONicePriority, err, executable)
 		}
 	}
 	if err = gionice.SetNicePri(0, gionice.PRIO_PROCESS, cfg.General.CPUNicePriority); err != nil {
-		log.Warnf("can't set CPU priority %v, error: %v", cfg.General.CPUNicePriority, err)
+		log.Warnf("can't set CPU priority %v, error: %v, use `sudo setcap cap_sys_nice+ep %s` to fix it", cfg.General.CPUNicePriority, err, executable)
 	}
 	return nil
 }
