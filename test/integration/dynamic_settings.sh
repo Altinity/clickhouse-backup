5e65b6b60cae814958f65b9315bfae50da0a89be
Slach <bloodjazman@gmail.com>
2024-11-19 16:01:01 +0400

    fix clickhouse-server memory optimizations
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- test/integration/dynamic_settings.sh
+++ test/integration/dynamic_settings.sh
@@ -474,7 +474,7 @@ EOT
 fi
 
 # blob_storage_log available in 23.11
-if [[ "$CLICKHOUSE_VERSION" == "head" || "${CLICKHOUSE_VERSION}" =~ ^23\.1[1-9] || "${CLICKHOUSE_VERSION}" =~ ^2[4-9\.[1-9] ]]; then
+if [[ "$CLICKHOUSE_VERSION" == "head" || "${CLICKHOUSE_VERSION}" =~ ^23\.1[1-9] || "${CLICKHOUSE_VERSION}" =~ ^2[4-9]\.[1-9] ]]; then
 
 cat <<EOT > /etc/clickhouse-server/config.d/blob_storage_log.xml
 <yandex>
@@ -492,18 +492,28 @@ EOT
 
 fi
 
-if [[ "$CLICKHOUSE_VERSION" == "head" || "${CLICKHOUSE_VERSION}" =~ ^19\.1[1-9] || "${CLICKHOUSE_VERSION}" =~ ^2[4-9\.[1-9] ]]; then
+if [[ "$CLICKHOUSE_VERSION" == "head" || "${CLICKHOUSE_VERSION}" =~ ^19\.1[1-9] || "${CLICKHOUSE_VERSION}" =~ ^2[1-9]\.[1-9] ]]; then
 
-cat <<EOT > /etc/clickhouse-server/confid.d/merge_tree_low_memory.xml
+cat <<EOT > /etc/clickhouse-server/config.d/merge_tree_low_memory.xml
 <yandex>
     <merge_tree>
         <merge_max_block_size>1024</merge_max_block_size>
         <max_bytes_to_merge_at_max_space_in_pool>1073741824</max_bytes_to_merge_at_max_space_in_pool>
-        <number_of_free_entries_in_pool_to_lower_max_size_of_merge>0
-        </number_of_free_entries_in_pool_to_lower_max_size_of_merge>
+        <number_of_free_entries_in_pool_to_lower_max_size_of_merge>0</number_of_free_entries_in_pool_to_lower_max_size_of_merge>
         <number_of_free_entries_in_pool_to_execute_mutation>1</number_of_free_entries_in_pool_to_execute_mutation>
-        <number_of_free_entries_in_pool_to_execute_optimize_entire_partition>1
-        </number_of_free_entries_in_pool_to_execute_optimize_entire_partition>
+    </merge_tree>
+</yandex>
+EOT
+
+fi
+
+
+if [[ "$CLICKHOUSE_VERSION" == "head" || "${CLICKHOUSE_VERSION}" =~ ^2[3]\.[4-9] || "${CLICKHOUSE_VERSION}" =~ ^2[4-9]\.[1-9] ]]; then
+
+cat <<EOT > /etc/clickhouse-server/config.d/merge_tree_low_memory_23.8.xml
+<yandex>
+    <merge_tree>
+        <number_of_free_entries_in_pool_to_execute_optimize_entire_partition>1</number_of_free_entries_in_pool_to_execute_optimize_entire_partition>
     </merge_tree>
 </yandex>
 EOT
