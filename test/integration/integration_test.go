24535ba058ecc46e20fd1427d62fdd98fd990779
Slach <bloodjazman@gmail.com>
2025-01-14 15:05:23 +0500

    add alibaba/oss/aliyuncs worked config and test   fix https://github.com/Altinity/clickhouse-backup/issues/877#issuecomment-2589164718),   fix https://github.com/Altinity/clickhouse-backup/issues/505#issuecomment-2589163706
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -739,6 +739,18 @@ func TestIntegrationS3(t *testing.T) {
 	env.Cleanup(t, r)
 }
 
+func TestIntegrationAlibabaOverS3(t *testing.T) {
+	if os.Getenv("QA_ALIBABA_SECRET_KEY") == "" {
+		t.Skip("Skipping Alibabacloud integration tests... QA_ALIBABA_SECRET_KEY missing")
+		return
+	}
+	env, r := NewTestEnvironment(t)
+	env.InstallDebIfNotExists(r, "clickhouse-backup", "gettext-base")
+	env.DockerExecNoError(r, "clickhouse-backup", "bash", "-xec", "cat /etc/clickhouse-backup/config-s3-alibabacloud.yml.template | envsubst > /etc/clickhouse-backup/config-s3-alibabacloud.yml")
+	env.runMainIntegrationScenario(t, "S3", "config-s3-alibabacloud.yml")
+	env.Cleanup(t, r)
+}
+
 func TestIntegrationGCS(t *testing.T) {
 	if isTestShouldSkip("GCS_TESTS") {
 		t.Skip("Skipping GCS integration tests...")
