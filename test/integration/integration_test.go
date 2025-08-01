ff3ccd94b8f70e65f149d21b7261050d1679df25
Slach <bloodjazman@gmail.com>
2025-08-01 22:30:32 +0400

    add TestListFormat, fix https://github.com/Altinity/clickhouse-backup/issues/1192
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -2311,37 +2311,42 @@ func TestSkipTablesAndSkipTableEngines(t *testing.T) {
 func TestListFormat(t *testing.T) {
 	env, r := NewTestEnvironment(t)
 	env.connectWithWait(t, r, 0*time.Second, 1*time.Second, 1*time.Minute)
-	
+
 	// Create a test backup to have something to list
 	env.DockerExecNoError(r, "clickhouse-backup", "bash", "-ce", "ALLOW_EMPTY_BACKUPS=true clickhouse-backup -c /etc/clickhouse-backup/config-s3.yml create test_list_format_backup")
-	
+
 	// Test text format (default)
 	out, err := env.DockerExecOut("clickhouse-backup", "clickhouse-backup", "-c", "/etc/clickhouse-backup/config-s3.yml", "list")
 	r.NoError(err)
 	r.Contains(out, "test_list_format_backup")
-	
+	r.Contains(out, "all:0B,data:0B,arch:0B,obj:0B,meta:0B,rbac:0B,conf:0B")
+
 	// Test JSON format
 	out, err = env.DockerExecOut("clickhouse-backup", "clickhouse-backup", "-c", "/etc/clickhouse-backup/config-s3.yml", "list", "--format", "json")
 	r.NoError(err)
-	r.Contains(out, "\"name\":")
-	r.Contains(out, "test_list_format_backup")
-	
+	r.Contains(out, "\"BackupName\":\"test_list_format_backup\"")
+	r.Contains(out, "\"Size\":\"all:0B,data:0B,arch:0B,obj:0B,meta:0B,rbac:0B,conf:0B\"")
+	r.Contains(out, "\"Description\":\"regular\",\"RequiredBackup\":\"\",\"Type\":\"local\"")
+
 	// Test YAML format
 	out, err = env.DockerExecOut("clickhouse-backup", "clickhouse-backup", "-c", "/etc/clickhouse-backup/config-s3.yml", "list", "--format", "yaml")
 	r.NoError(err)
-	r.Contains(out, "name:")
-	r.Contains(out, "test_list_format_backup")
-	
+	r.Contains(out, "- backupname: test_list_format_backup")
+	r.Contains(out, "size: all:0B,data:0B,arch:0B,obj:0B,meta:0B,rbac:0B,conf:0B")
+	r.Contains(out, "description: regular")
+	r.Contains(out, "requiredbackup: \"\"")
+	r.Contains(out, "type: local")
+
 	// Test CSV format
 	out, err = env.DockerExecOut("clickhouse-backup", "clickhouse-backup", "-c", "/etc/clickhouse-backup/config-s3.yml", "list", "--format", "csv")
 	r.NoError(err)
 	r.Contains(out, "test_list_format_backup")
-	
+
 	// Test TSV format
 	out, err = env.DockerExecOut("clickhouse-backup", "clickhouse-backup", "-c", "/etc/clickhouse-backup/config-s3.yml", "list", "--format", "tsv")
 	r.NoError(err)
 	r.Contains(out, "test_list_format_backup")
-	
+
 	// Clean up
 	env.DockerExecNoError(r, "clickhouse-backup", "clickhouse-backup", "-c", "/etc/clickhouse-backup/config-s3.yml", "delete", "local", "test_list_format_backup")
 	env.Cleanup(t, r)
