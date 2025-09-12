a06d7049874a0d1770b5738ea9a001dd35eee95d
Slach <bloodjazman@gmail.com>
2025-09-12 12:29:56 +0500

    fix TestEmbeddedGCSOverS3, fix https://github.com/Altinity/clickhouse-backup/issues/1237
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -721,18 +721,20 @@ func TestEmbeddedGCSOverS3(t *testing.T) {
 	if compareVersion(version, "23.3") < 0 {
 		t.Skipf("Test skipped, BACKUP/RESTORE not production ready for %s version, look https://github.com/ClickHouse/ClickHouse/issues/39416 for details", version)
 	}
-	t.Logf("@TODO RESTORE Ordinary with old syntax still not works for %s version, look https://github.com/ClickHouse/ClickHouse/issues/43971", os.Getenv("CLICKHOUSE_VERSION"))
-	env, r := NewTestEnvironment(t)
+	if compareVersion(version, "23.3") >= 0 && compareVersion(version, "24.3") < 0 {
+		t.Logf("@TODO RESTORE Ordinary with old syntax still not works for %s version, look https://github.com/ClickHouse/ClickHouse/issues/43971", os.Getenv("CLICKHOUSE_VERSION"))
+	}
 
 	// === GCS over S3 ===
 	if compareVersion(version, "24.3") >= 0 && os.Getenv("QA_GCS_OVER_S3_BUCKET") != "" {
+		env, r := NewTestEnvironment(t)
 		//@todo think about named collections to avoid show credentials in logs look to https://github.com/fsouza/fake-gcs-server/issues/1330, https://github.com/fsouza/fake-gcs-server/pull/1164
 		env.InstallDebIfNotExists(r, "clickhouse-backup", "ca-certificates", "gettext-base")
 		env.DockerExecNoError(r, "clickhouse-backup", "bash", "-xec", "cat /etc/clickhouse-backup/config-gcs-embedded-url.yml.template | envsubst > /etc/clickhouse-backup/config-gcs-embedded-url.yml")
 		env.runMainIntegrationScenario(t, "EMBEDDED_GCS_URL", "config-gcs-embedded-url.yml")
+		env.Cleanup(t, r)
 	}
 
-	env.Cleanup(t, r)
 }
 
 func TestEmbeddedS3(t *testing.T) {
@@ -3788,7 +3790,7 @@ func (env *TestEnvironment) checkObjectStorageIsEmpty(t *testing.T, r *require.A
 		r.NoError(err, "%s\nunexpected checkRemoteDir error: %v", out, err)
 		r.Equal(expected, strings.Trim(out, "\r\n\t "))
 	}
-	if remoteStorageType == "S3" || remoteStorageType == "S3_EMBEDDED_URL" {
+	if remoteStorageType == "S3" || remoteStorageType == "S3_EMBEDDED_URL" || strings.Contains(remoteStorageType, "EMBEDDED") {
 		checkRemoteDir("total 0", "minio", "bash", "-c", "ls -lh /bitnami/minio/data/clickhouse/")
 	}
 	if remoteStorageType == "SFTP" {
