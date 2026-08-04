f717ce7282bb2797373ba3839b21653b0602ae62
slach <bloodjazman@gmail.com>
2026-08-04 06:10:19 +0400

    fix e2e tests, fix  https://github.com/Altinity/clickhouse-backup/issues/1492

--- test/integration/uploadStaleResumeState_test.go
+++ test/integration/uploadStaleResumeState_test.go
@@ -58,8 +58,8 @@ func TestUploadStaleResumeStateAfterRemoteDelete(t *testing.T) {
 	// the only reliable check that data was really uploaded is a full restore from remote
 	env.DockerExecNoError(r, "clickhouse-backup", "clickhouse-backup", "-c", "/etc/clickhouse-backup/"+configFile, "delete", "local", backupName)
 	dropSuffix := ""
-	if compareVersion(os.Getenv("CLICKHOUSE_VERSION"), "20.3") > 0 {
-		dropSuffix = " NO DELAY"
+	if compareVersion(os.Getenv("CLICKHOUSE_VERSION"), "20.4") >= 0 {
+		dropSuffix = " SYNC"
 	}
 	env.queryWithNoError(t, r, "DROP DATABASE "+dbName+dropSuffix)
 	env.DockerExecNoError(r, "clickhouse-backup", "clickhouse-backup", "-c", "/etc/clickhouse-backup/"+configFile, "restore_remote", "--rm", backupName)
